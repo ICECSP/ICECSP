b6dbed8c01c1e756bd8d8397bead4d8cd976992f
code-scanner <cyber.gupta001@gmail.com>
2023-09-28 17:17:49 +0530

    landing page changes

--- components/homePage/LandingPage.js
+++ components/homePage/LandingPage.js
@@ -5,18 +5,17 @@ export default function LandingPage() {
     return <>
         <div className="w-full h-[80vh] relative overflow-hidden">
             <div className="flex h-full relative z-10">
-                <div className="w-1/4 h-full bg-white/50">
+                <div className="w-1/4 h-full bg-white/50"/>
 
-                </div>
                 <div className="w-3/4 h-full bg-black/80 p-8 px-32 flex flex-col">
-                    <span className="text-white font-bold text-5xl leading-relaxed" >International Conference on Electronics, Communication and Signal Processing 
-                    <br/><span className="text-[1.4em]">ICECSP 2024</span>
+                    <span className="text-white text-center font-bold text-5xl leading-relaxed">
+                        International Conference on Electronics, Communication and Signal Processing
                     </span>
+                    <button className="my-12 w-fit p-6 text-3xl font-bold mx-auto text-white border-4 border-white hover:bg-white hover:text-black transition-colors rounded-lg">
+                        ICECSP 2024
+                    </button>
                 </div>
 
-                <div>
-
-                </div>
 
             </div>
             <div className="w-full bottom-0 absolute">
