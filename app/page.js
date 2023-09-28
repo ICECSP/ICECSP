9451ece32216d3cce144b33182365c6a7ed70b40
code-scanner <cyber.gupta001@gmail.com>
2023-09-28 16:52:31 +0530

    navbar in all page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import LandingPage from "@/components/homePage/LandingPage"
-import NavBar from "@/components/NavBar"
 
 export default function Home() {
   return (
    <>
-    <NavBar></NavBar>
     <LandingPage/>
    </>
   )
