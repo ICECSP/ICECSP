444aa6f729a0d92d29016ef3469a87854b434d91
code-scanner <cyber.gupta001@gmail.com>
2023-09-28 15:31:27 +0530

    tailwind color pallete

--- components/homePage/LandingPage.js
+++ components/homePage/LandingPage.js
@@ -4,6 +4,15 @@ import landingBg from "@/public/images/landingBg.jpg"
 export default function LandingPage() {
     return <>
         <div className="w-full h-[80vh] relative overflow-hidden">
+            <div className="flex h-full">
+                <div className="w-1/2 h-full">
+
+                </div>
+                <div className="w-1/2">
+
+                </div>
+
+            </div>
             <div className="w-full bottom-0 absolute">
                 <Image src = {landingBg} />
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primaryRegular: "#309c86",
+        primaryLight: "#48bdad",
+        primaryDark: "#073d27",
+        primary90: "#1f7d64",
+        primary80: "#58d0c5",
+        primary50: "#83ded8",
+        primary20: "#b7edea",
+        primary10: "#d3f5f3",
+        primary05: "#f5fcfc",
+        black: "#1E1E24",
+        white: "#F4F4F4",
+      },
     },
   },
   plugins: [],
