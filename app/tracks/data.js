ff564b92a238753d3e07fb63ad0f099113c15595
Utkarsh Trivedi <trivediutkarsh31@gmail.com>
2023-09-29 14:38:10 +0530

    tracks data

---
+++ app/tracks/data.js
@@ -0,0 +1,81 @@
+export const tracks = [
+    {
+        heading: "Microelectronic Circuits and Systems",
+        Support: [
+            "IEEE Electron Device Society",
+            "IEEE Photonics Society",
+            "IEEE Circuits and Systems Society"
+        ],
+
+        Subtracks:
+            [
+                "Electronic Device Physics",
+                "Photonic / Opto-electronic Devices",
+                "Circuits and Systems",
+                "Nano-electronics",
+                "System Level Design",
+                "Electronics for Green Technology",
+                "Nano scale device Modeling",
+                "Device processing Techniques"
+            ],
+        id:"microelectronics"
+    },
+    {
+        heading: "Communication Systems and Networks",
+        Support: [
+            "IEEE Communication Society"
+        ],
+
+        Subtracks: [
+            "Optical Fibre Communication and Systems",
+            "Computer and Intelligent Communication",
+            "Wireless and Broadband Communication and Sensor Networks",
+            "Network Security",
+            "RF Antenna Design and Modeling",
+            "Cognitive Communications",
+            "Analog/Digital Communication Systems",
+            "LTE, 5G and 6G Design"
+        ],
+        id:"communication"
+
+    },
+    {
+        heading: "Signal and Image Processing",
+        Support: [
+            "IEEE Signal Processing Society"
+        ],
+
+        Subtracks: [
+            "Signal Processing Systems and Algorithms",
+            "Radar Signal Processing",
+            "Speech and Audio Signal Processing and Coding",
+            "Image Signal Processing",
+            "Biomedical Signal Processing and Bioinformatics",
+            "Remote sensing Applications",
+            "Signal Processing Applications",
+            "Computer and Intelligent Communication"
+        ],
+        id:"signal"
+    },
+    {
+        heading: "VLSI Technology and Embedded Systems",
+        Support: [
+            "IEEE Electron Device Society",
+            "IEEE Circuits and Systems Society"
+        ],
+
+        Subtracks: [
+            "VLSI Design",
+            "High Speed/Low Power Circuits",
+            "Digital Circuits and FPGA Based Designs",
+            "Analog/Mixed Signal System Design",
+            "Deep Submicron Design and Modeling Issues",
+            "Testing and Verification",
+            "Emerging Semiconductor Technologies",
+            "Real-Time Embedded Systems and Applications",
+            "Distributed Embedded Computing",
+            "Internet-of-Things"
+        ],
+        id:"vlsi"
+    },
+]
--- app/tracks/page.js
+++ app/tracks/page.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function Tracks() {
+  const trackOptions = [
+    { name: "MICROELECTRONIC CIRCUITS AND SYSTEMS", link: "tracks/microelectronic" },
+    {
+      name: "COMMUNICATION SYSTEMS AND NETWORKS",
+      link: "tracks/communication",
+    },
+    { name: "SIGNAL PROCESSING AND APPLICATIONS", link: "tracks/signal" },
+    {
+      name: "VLSI TECHNOLOGY AND APPLICATIONS",
+      link: "tracks/vlsi",
+    },
+  ];
   return (
-    <>
-    This is tracks page
-    </>
-  )
+    <div>
+
+    </div>
+  );
 }
