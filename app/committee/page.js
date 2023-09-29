45eae64aa0255a68bad6bceefefe836af2b68f58
Utkarsh Trivedi <trivediutkarsh31@gmail.com>
2023-09-29 16:04:56 +0530

    committee page

--- app/committee/page.js
+++ app/committee/page.js
@@ -1,9 +1,14 @@
 import Committee from "@/components/committeePage/Committees"
-import {cheifPatron} from '@/components/committeePage/data'
+import {cheifPatron, internationalAdvisoryCommittee, technicalProgramCommittee ,nationalAdvisoryCommittee, localOrganisingCommittee} from '@/components/committeePage/data'
 export default function Committees() {
     return (
         <> 
-            <Committee data = {cheifPatron} even = {true}/>
+            <Committee data = {cheifPatron} even={true}/>
+            <Committee data={technicalProgramCommittee} even={false}/>
+            <Committee data = {internationalAdvisoryCommittee} even={true}/>
+            <Committee data = {nationalAdvisoryCommittee} even={false}/>
+            <Committee data = {localOrganisingCommittee} even={true}/>
+           
         </>
     )
 }
--- components/committeePage/data.js
+++ components/committeePage/data.js
@@ -2,7 +2,7 @@ import profile from "@/public/images/profile.png"
 
 export const cheifPatron = [
     {
-        type: "Cheif Patron",
+        type: "HONORARY CHAIR",
         members: [
             {
                 name: 'Prof. (Dr.) Ajay K. Sharma',
@@ -13,3 +13,184 @@ export const cheifPatron = [
         ]
     }
 ]
+
+export const technicalProgramCommittee= [
+    {
+        type: "TECHNICAL PROGRAM COMMITTEE",
+        members: [
+            {
+                name: 'Prof. Mukesh Singhal',
+                designation: 'Chancellor\'s Professor',
+                affiliation: { line1: 'University of California at Merced', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Hiroyuki Tsuda',
+                affiliation: { line1: 'Keio University, Japan', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Ali Gharsallah',
+                affiliation: { line1: 'Univ. of Tunis E. M., Tunisia', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Isao Echizen',
+                affiliation: { line1: 'Director, National Institute Of Informatics, Tokyo, Japan', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Yudong Zhang',
+                affiliation: { line1: 'University Of Leicester, UK', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. M. Yasin Akhtar Raja',
+                designation: 'Professor',
+                affiliation: { line1: 'University of North Carolina at Charlotte, USA', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Sergii Ubizskii',
+                affiliation: { line1: 'LPNU, Ukraine', line2: '' },
+                photo: profile
+            },
+        ]
+    }
+]
+    
+export const internationalAdvisoryCommittee= [
+    {
+        type: "INTERNATIONAL ADVISORY COMMITTEE",
+        members: [
+            {
+                name: 'Prof. Mukesh Singhal',
+                designation: 'Chancellor\'s Professor',
+                affiliation: { line1: 'University of California at Merced', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Hiroyuki Tsuda',
+                affiliation: { line1: 'Keio University, Japan', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Ali Gharsallah',
+                affiliation: { line1: 'Univ. of Tunis E. M., Tunisia', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Isao Echizen',
+                affiliation: { line1: 'Director, National Institute Of Informatics, Tokyo, Japan', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Yudong Zhang',
+                affiliation: { line1: 'University Of Leicester, UK', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. M. Yasin Akhtar Raja',
+                designation: 'Professor',
+                affiliation: { line1: 'University of North Carolina at Charlotte, USA', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Sergii Ubizskii',
+                affiliation: { line1: 'LPNU, Ukraine', line2: '' },
+                photo: profile
+            },
+        ]
+    }
+]
+    
+export const nationalAdvisoryCommittee= [
+    {
+        type: "NATIONAL ADVISORY COMMITTEE",
+        members: [
+            {
+                name: 'Prof. Mukesh Singhal',
+                designation: 'Chancellor\'s Professor',
+                affiliation: { line1: 'University of California at Merced', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Hiroyuki Tsuda',
+                affiliation: { line1: 'Keio University, Japan', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Ali Gharsallah',
+                affiliation: { line1: 'Univ. of Tunis E. M., Tunisia', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Isao Echizen',
+                affiliation: { line1: 'Director, National Institute Of Informatics, Tokyo, Japan', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Yudong Zhang',
+                affiliation: { line1: 'University Of Leicester, UK', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. M. Yasin Akhtar Raja',
+                designation: 'Professor',
+                affiliation: { line1: 'University of North Carolina at Charlotte, USA', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Sergii Ubizskii',
+                affiliation: { line1: 'LPNU, Ukraine', line2: '' },
+                photo: profile
+            },
+        ]
+    }
+]
+    
+export const localOrganisingCommittee= [
+    {
+        type: "LOCAL ORGANISING COMMITTEE",
+        members: [
+            {
+                name: 'Prof. Mukesh Singhal',
+                designation: 'Chancellor\'s Professor',
+                affiliation: { line1: 'University of California at Merced', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Hiroyuki Tsuda',
+                affiliation: { line1: 'Keio University, Japan', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Ali Gharsallah',
+                affiliation: { line1: 'Univ. of Tunis E. M., Tunisia', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Isao Echizen',
+                affiliation: { line1: 'Director, National Institute Of Informatics, Tokyo, Japan', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Yudong Zhang',
+                affiliation: { line1: 'University Of Leicester, UK', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. M. Yasin Akhtar Raja',
+                designation: 'Professor',
+                affiliation: { line1: 'University of North Carolina at Charlotte, USA', line2: '' },
+                photo: profile
+            },
+            {
+                name: 'Prof. Sergii Ubizskii',
+                affiliation: { line1: 'LPNU, Ukraine', line2: '' },
+                photo: profile
+            },
+        ]
+    }
+]
+    
