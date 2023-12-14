05640e07d82be7eae1a03b66fb7760ce58914670
code-scanner <cyber.gupta001@gmail.com>
2023-12-14 11:44:56 +0530

    added ieee images

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,7 +3,11 @@ import { useState } from "react";
 import Link from "next/link";
 
 import nitdLogo from "@/public/images/nitd-logo.png";
-import ieeeLogo from "@/public/images/ieee.jpeg";
+import ieeeLogo from "@/public/images/ieee/ieee.jpeg";
+import IEEEPhotonic from "@/public/images/ieee/1.jpeg"
+import IEEECircuits from "@/public/images/ieee/2.jpg"
+import IEEEElectron from "@/public/images/ieee/3.jpg"
+
 import Image from "next/image";
 export default function NavBar() {
   const [menu, setMenu] = useState(false);
@@ -13,7 +17,7 @@ export default function NavBar() {
         <Header menu={menu} setMenu={setMenu} />
       </header>
       <div className={`${menu ? "" : "hidden"} md:block`}>
-        <NavRibbon setMenu = {()=>setMenu(!menu)} />
+        <NavRibbon setMenu={() => setMenu(!menu)} />
       </div>
     </>
   );
@@ -32,7 +36,7 @@ const links = [
   { name: "Contact", link: "/contact" },
 ];
 
-const NavRibbon = ({setMenu}) => {
+const NavRibbon = ({ setMenu }) => {
   return (
     <nav className="bg-primary90 sm:bg-primary80 text-white sticky top-0 z-50">
       <div className="py-2 sm:py-3 px-6 left-0 w-full md:w-auto mx-auto ">
@@ -64,15 +68,45 @@ const Header = ({ menu, setMenu }) => {
         />
         <span className="hidden sm:block sm:text-3xl text-2xl font-bold text-primaryDark">ICECSP 2024</span>
       </div>
-      <div className="space-y-2 ml-auto mr-8 sm:mr-0">
-        <Image
-          src={ieeeLogo}
-          className="object-contain w-28 mx-auto sm:w-36 "
-          alt="IEEE"
-        />
-        <div className="hidden sm:block text-xs text-center font-semibold text-gray-500">Technical Co-Sponsor</div>
+      <div className="flex gap-x-4 items-center">
+
+        <div className="ml-auto">
+          <Image
+            src={IEEEPhotonic}
+            title="IEEEPhotonic Society Delhi Section Chapter, Rajasthan"
+            className="object-contain w-28 mx-auto sm:w-44 "
+            alt="IEEE"
+          />
+          {/* <div className="hidden sm:block text-xs text-center font-semibold text-gray-500">Technical Co-Sponsor</div> */}
+        </div>
+        <div className="">
+          <Image
+            src={IEEECircuits}
+            title="IEEE Circuits and Systems Society Delhi Chapter"
+            className="object-contain w-28 mx-auto sm:w-20 "
+            alt="IEEE"
+          />
+          {/* <div className="hidden sm:block text-xs text-center font-semibold text-gray-500">Technical Co-Sponsor</div> */}
+        </div>
+        <div className="">
+          <Image
+            src={IEEEElectron}
+            title="IEEE Electron Device Society Delhi Chapter"
+            className="object-contain w-28 mx-auto sm:w-44 "
+            alt="IEEE"
+          />
+          {/* <div className="hidden sm:block text-xs text-center font-semibold text-gray-500">Technical Co-Sponsor</div> */}
+        </div>
+        <div className="">
+          <Image
+            src={ieeeLogo}
+            className="object-contain w-28 mx-auto sm:w-48 "
+            alt="IEEE"
+          />
+          <div className="hidden sm:block text-xs text-center font-semibold text-gray-500">Technical Co-Sponsor</div>
+        </div>
       </div>
-      <div className="md:hidden h-8 w-8 fill-slate-700" onClick={()=>setMenu(!menu)}>
+      <div className="md:hidden h-8 w-8 fill-slate-700" onClick={() => setMenu(!menu)}>
         {menu ?
           <svg className="transform rotate-45" height="32px" version="1.1" viewBox="0 0 32 32" width="32px" xmlns="http://www.w3.org/2000/svg"><path d="M28,14H18V4c0-1.104-0.896-2-2-2s-2,0.896-2,2v10H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h10v10c0,1.104,0.896,2,2,2  s2-0.896,2-2V18h10c1.104,0,2-0.896,2-2S29.104,14,28,14z" /></svg> :
           <svg xmlns="http://www.w3.org/2000/svg" height="32px" version="1.1" viewBox="0 0 32 32" width="32px"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" /></svg>
