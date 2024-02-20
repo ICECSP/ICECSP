a36ca360de74a2ee65a6643bee82eb4a152f6395
rajRishi22 <rishiraj14ap2004raj@gmail.com>
2024-02-20 17:10:03 +0530

    Instructions added on SubmissionsPage

--- components/submissionPage/Submission.js
+++ components/submissionPage/Submission.js
@@ -6,63 +6,50 @@ export default function SubmissionProcess() {
       </h2>
       <ul className="leading-6 sm:leading-10 text-justify list-disc">
         <li className="mb-4">
-        All the paper submissions will be done through the Microsoft CMT
-conference management system as per the link provided under
-submission guidelines.
+          All the paper submissions will be done through the Microsoft CMT
+          conference management system as per the link provided under
+          submission guidelines.
         </li>
         <li className="mb-4">
-        Comments of the reviewers will be directed to the authors
-sufficiently before the camera-ready paper submission deadline so
-that they can be accommodated in the final camera-ready paper.
+          Comments of the reviewers will be directed to the authors sufficiently before the camera-ready paper submission deadline so that they can be accommodated in the final camera-ready paper.
         </li>
         <li className="mb-4">
-        Prospective authors are invited to submit full (original) research
-papers (which are NOT submitted/published/under consideration
-anywhere in other conferences/journals) in electronic (pdf,
-doc/docx) format through the Link provided on the website.
+          Prospective authors are invited to submit full (original) research papers (which are NOT submitted/published/under consideration anywhere in other conferences/journals) in electronic (pdf, doc/docx) format through the Link provided on the website.
         </li>
         <li className="mb-4">
-        Prospective authors are requested to submit full length paper no
-longer than six (6) pages. Additional charges will be levied for each
-additional page. The page limit is inclusive of references.
+          Prospective authors are requested to submit full-length paper no longer than six (6) pages. Additional charges will be levied for each additional page. The page limit is inclusive of references.
         </li>
         <li className="mb-4">
-        At least one author needs to present the paper at the conference
-after acceptance. If he/she fails to present, the paper will not be
-published in the proceedings.
+          At least one author needs to present the paper at the conference after acceptance. If he/she fails to present, the paper will not be published in the proceedings.
         </li>
       </ul>
+      
       <h4 className="text-primary90 text-2xl sm:text-3xl text-center font-bold mb-8 mt-0 sm:mt-8">
         Minimum Specific Criteria for Selecting Papers
       </h4>
       <ul className="leading-6 sm:leading-10 text-justify list-disc">
         <li className="mb-4">
-        Plagiarism check using the (Turnitin) and paper will accept if the
-similarity index in with threshold limits of acceptance (less than
-15%-20%)
+          Plagiarism check using the (Turnitin) and paper will accept if the similarity index in with threshold limits of acceptance (less than 15%-20%)
         </li>
         <li className="mb-4">
-        On the basis of received review reports, decision of “Acceptance” or
-“Rejection” will be made.
+          On the basis of received review reports, decision of “Acceptance” or “Rejection” will be made.
         </li>
         <li className="mb-4">
-        The submitted paper must be relevant to the field of tracks and sub-
-tracks proposed.
+          The submitted paper must be relevant to the field of tracks and sub-tracks proposed.
         </li>
         <li className="mb-4">
-        The review process will be blind for authors.
+          The review process will be blind for authors.
         </li>
       </ul>
-<h2 className="text-primary90 text-3xl sm:text-4xl text-center font-bold mb-8 mt-12">
+      <h2 className="text-primary90 text-3xl sm:text-4xl text-center font-bold mb-8 mt-12">
         Submission Guidelines
       </h2>
       <p className="text-justify mb-4">
-        Interested authors are invited to submit full papers, including results, figures, and
-        references. Papers will be accepted only by electronic submission through{" "}
+        Interested authors are invited to submit full papers, including results, figures, and references. Papers will be accepted only by electronic submission through{" "}
         <a target="_blank" href="https://cmt3.research.microsoft.com/ICECSP2024" className="text-primary90 underline">
           Microsoft CMT link
         </a>{" "}
-        given below
+        given below.
       </p>
       <p className="text-justify mb-4">
         <strong>Download Conference Template:</strong>{" "}
@@ -71,19 +58,66 @@ tracks proposed.
         </a>
       </p>
       <p className="text-justify mb-4">
-        All manuscripts submitted to ICECSP 2024 will follow the triple-blind review
-        process.
+        All manuscripts submitted to ICECSP 2024 will follow the triple-blind review process.
       </p>
+        
+      <h4 className="text-primary90 text-2xl sm:text-3xl text-center font-bold mb-4 mt-8">
+        Instructions
+      </h4>
+      <p className="text-justify mb-4">
+  All the applicants must have to be submitted the pdf version of the paper which is only process by the IEEE PDF Xpress site, as per the guidelines submitted below.
+</p>
+<p className="text-justify mb-4">
+  <strong>PDF Instructions for your Authors</strong>
+</p>
+<p className="text-justify mb-4">
+  2024 First International Conference on Electronics, Communication and Signal Processing (ICECSP)
+</p>
+<p className="text-justify mb-4">
+  <strong>Before Creating a PDF</strong>
+  <br />
+  Add the copyright notice to the bottom of the first page of your source document. If necessary, contact Manoj kumar at <a href="mailto:manojtaleja@nitdelhi.ac.in" className="text-primary90 underline">manojtaleja@nitdelhi.ac.in</a> for the appropriate copyright notice. Add the copyright notice to the bottom of the first page of your source document. If necessary, contact Jasdeep Dhanoa at <a href="mailto:jasdeep@igdtuw.ac.in" className="text-primary90 underline">jasdeep@igdtuw.ac.in</a> for the appropriate copyright notice.
+  Proofread your source document thoroughly to confirm that it will require no revision.
+</p>
+
+<p className="text-justify mb-4">
+  <strong>Creating your PDF eXpress Account</strong>
+  <br />
+  Log in to the <a href="https://ieee-pdf-express.org/" className="text-primary90 underline">IEEE PDF eXpress TM site.</a>  
+  <br />
+  First-time users should do the following:
+</p>
+<ul className="list-disc pl-6">
+  <li>Select the New Users - Click Here link.</li>
+    <li>Enter the following:
+      <ul className="list-disc pl-6">
+        <li>61809X for the Conference ID</li>
+        <li>your email address</li>
+        <li>a password</li>
+      </ul>
+    </li>
+    <li>Continue to enter information as prompted.</li>
+    <li>An Online confirmation will be displayed and an email confirmation will be sent verifying your account setup.</li>
+  </ul>
+  <p className="text-justify mb-4">  Previous users of PDF eXpress need to follow the above steps, but should enter the same password that was used for previous conferences. Verify that your contact information is valid.
+</p>
+<p className="text-justify mb-4">
+  <strong>Contacting PDF eXpress Support</strong>
+  <br />
+  Access the <a href="https://supportcenter.ieee.org/app/answers/list/p/12" className="text-primary90 underline">Publications Support Center</a>  for IEEE PDF eXpress.
+  <br />
+  If you do not find an answer in the Find Answers tab, go to the Ask A Question tab.
+  <br />
+  Your question will be forwarded to IEEE PDF Support and answered within 3 business days.
+</p>  
+      
       <h4 className="text-primary90 text-2xl sm:text-3xl text-center font-bold mb-4 mt-8">
         Plagiarism Policy
       </h4>
       <p className="text-justify mb-4">
-        ICECSP 2024 will take plagiarism very seriously and regard plagiarism as a professional
-        misconduct. All submissions will be screened for plagiarism with a permissible limit of 15
-        - 20% through
-          Turnitin Software
-        . Any instance surpassing this threshold will lead to immediate rejection.
+        ICECSP 2024 will take plagiarism very seriously and regard plagiarism as a professional misconduct. All submissions will be screened for plagiarism with a permissible limit of 15 - 20% through Turnitin Software. Any instance surpassing this threshold will lead to immediate rejection.
       </p>
     </div>
   );
 }
+
