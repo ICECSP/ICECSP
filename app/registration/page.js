5591f5de78b071a59d6147fb712aa695b4787054
kartikg1809 <kartikg1809@gmail.com>
2024-06-02 18:50:42 +0530

    Added bank details for registration

--- app/registration/page.js
+++ app/registration/page.js
@@ -3,20 +3,62 @@ import {
   internationalFee,
 } from "@/components/registrationFeePage/data";
 import RegistrationFee from "@/components/registrationFeePage/RegistrationFee";
-import RegistrationForm from "@/components/registrationFeePage/RegistrationForm";
 
 export default function Registration() {
   return (
     <>
-      <div className="mt-6 bg-primary05 p-6 rounded-sm pl-20">
-      <strong className="text-primary90 text-4xl p-4">Click here for registration:</strong>
+      <div className="dark:bg-gray-100  p-6 rounded-sm pl-20">
+        <strong className="text-primary90 text-4xl">
+          Click here for registration:
+        </strong>
         <a
           href="https://forms.gle/smtYAjf3oTLRJqat6"
           target="_blank"
-          class="text-primary90 text-4xl hover:underline hover:text-primary-dark "
-        >Registration Form
+          className="text-primary90 text-4xl underline p-2"
+        >
+          Registration Form
         </a>
+
+        <p className="mt-4 text-primary90 text-xl">
+          Every candidate must have to upload the duly signed registration form
+          at the last step of online registration.
+          <a
+            href="/docs/REGISTRATION_FORM.pdf"
+            className="text-primary90 underline p-2"
+            download
+          >
+            Template
+          </a>
+        </p>
+
+        <p className="mt-4 text-primary90 text-xl">
+          Bank Details to for Fee Transaction/RTGS/NEFT to deposit the requisite
+          registration fee, as per the registration fee mentioned:
+        </p>
+
+        <ul className="mt-2 text-primary90 text-lg list-disc pl-8">
+          <li>
+            <strong>Account Name:</strong> ICECSP 2024 NIT DELHI
+          </li>
+          <li>
+            <strong>Bank:</strong> ICICI Bank
+          </li>
+          <li>
+            <strong>Address:</strong> PLOT NO FA 7 ZONE P1 GT, KARNAL ROAD, NEW
+            DELHI DELHI - INDIA - 110036
+          </li>
+          <li>
+            <strong>A/C Number:</strong> 092901001889
+          </li>
+          <li>
+            <strong>MICR:</strong> 110229103
+          </li>
+          <li>
+            <strong>IFSC:</strong> ICIC0000929
+          </li>
+        </ul>
       </div>
+
       <div className="container mx-auto mt-5">
         <h1 className="text-primary90 text-4xl text-center font-bold my-8">
           Registration Fees
@@ -30,7 +72,7 @@ export default function Registration() {
           Foreign Authors
         </h2>
         <RegistrationFee data={internationalFee} />
-        <h1 className="mx-5 p-10 text-center font-semibold text-xl text-white">
+        <h1 className="mx-5 p-10 text-center font-semibold text-xl">
           *Note: Additional page charges (INR Rs. 500/ per extra pages are
           required for submitting paper of more than 6 pages)
         </h1>
