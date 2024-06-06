b59d67d1aacbbb5958d073f7d10241075e5302e9
kartikg1809 <kartikg1809@gmail.com>
2024-06-06 21:59:51 +0530

    Changed accomodations page

--- components/accomodationPage/Accomodation.js
+++ components/accomodationPage/Accomodation.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Accomodations() {
     return (
         <div>
-            <div className="dark:bg-gray-100  p-6 rounded-sm pl-20 pr-20">
-            <p>NIT Delhi will not be able to provide any accommodation during the ICECSP 2024 conference, 
+            <div className="p-6 rounded-sm pl-20 pr-20">
+            <p className='py-6'>NIT Delhi will not be able to provide any accommodation during the ICECSP 2024 conference, 
             however, for the convenience of participants, details of few nearby hotels are enlisted below.</p>
         <strong className="text-primary90 text-4xl">
           Click here for Accomodations:
