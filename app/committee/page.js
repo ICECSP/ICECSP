bad47263e7befd3f220a8b0dd184ce004a914ab1
kartikg1809 <kartikg1809@gmail.com>
2024-07-25 15:43:13 +0530

    Changes orders in committees page

--- app/committee/page.js
+++ app/committee/page.js
@@ -22,22 +22,28 @@ export default function Committees() {
   return (
     <>
       <Committee data={chiefPatron} even={true} />
-      <Committee data={honoraryChair} even={false} />
-      <Committee data={conferenceGeneralChair} even={true} />
-      <Committee data={conferenceChair} even={false} />
-      <Committee data={organizingSecretary} even={true} />
-      <Committee data={oversightCommittee} even={false} />
-      <Committee data={steeringCommittee} even={true} />
-      <Committee data={technicalProgramChair} even={false} />
-      <Committee data={internationalAdvisoryCommittee} even={true} onlyData = {true}/>
-      <Committee data={nationalAdvisoryCommittee} even={false} onlyData = {true}/>
-      <Committee data={technicalProgramCommittee} even={true} onlyData = {true}/>
-      <Committee data={localOrganisingCommittee} even={false} onlyData = {true}/>
-      <Committee data={publicationCommittee} even={true} onlyData = {true}/>
-      <Committee data={websiteUpdationCommittee} even={false} onlyData = {true}/>
-      <Committee data={hospitality} even={true} onlyData = {true}/>
-      <Committee data={financeChair} even={false} onlyData = {true}/>
-      <Committee data={purchase} even={true} onlyData = {true}/>
+      <div className="flex flex-wrap">
+        <div className="w-full md:w-1/2">
+          <Committee data={honoraryChair} even={false} />
+        </div>
+        <div className="w-full md:w-1/2">
+          <Committee data={conferenceGeneralChair} even={false} />
+        </div>
+      </div>
+      <Committee data={conferenceChair} even={true} />
+      <Committee data={organizingSecretary} even={false} />
+      <Committee data={oversightCommittee} even={true} />
+      <Committee data={steeringCommittee} even={false} />
+      <Committee data={technicalProgramChair} even={true} />
+      <Committee data={internationalAdvisoryCommittee} even={false} onlyData = {true}/>
+      <Committee data={nationalAdvisoryCommittee} even={true} onlyData = {true}/>
+      <Committee data={technicalProgramCommittee} even={false} onlyData = {true}/>
+      <Committee data={localOrganisingCommittee} even={true} onlyData = {true}/>
+      <Committee data={publicationCommittee} even={false} onlyData = {true}/>
+      <Committee data={websiteUpdationCommittee} even={true} onlyData = {true}/>
+      <Committee data={hospitality} even={false} onlyData = {true}/>
+      <Committee data={financeChair} even={true} onlyData = {true}/>
+      <Committee data={purchase} even={false} onlyData = {true}/>
     </>
   );
 }
