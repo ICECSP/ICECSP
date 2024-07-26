4e0799696367ec2acf112fa9d5234b6ff4b56a85
kartikg1809 <kartikg1809@gmail.com>
2024-07-26 19:54:57 +0530

    Updated schedule

--- components/homePage/LandingPage.js
+++ components/homePage/LandingPage.js
@@ -18,8 +18,8 @@ export default function LandingPage() {
               </h1>
 
               <h1 className="bg-black/80 text-sm sm:text-xl w-full mx-auto p-5 m-2 ">
-              This is a very tentative programme schedule and will be updated from time to time. Hence, applicants are kindly requested to keep on viewing in regular intervals. In case of any query, please drop an email to <a href="mailto:icecsp2024@nitdelhi.ac.in" className="text-blue-500">icecsp2024@nitdelhi.ac.in</a>.
-              <p className="blue-300"> <a href="..\docs\PROGRAM_SCHEDULE.pdf" className="text-blue-500">Tentative Programme Schedule</a> </p>
+              This is the 2nd draft of the updated programme schedule and will be updated from time to time. Hence, applicants are kindly requested to keep on viewing in regular intervals. In case of any query, please drop an email to <a href="mailto:icecsp2024@nitdelhi.ac.in" className="text-blue-500">icecsp2024@nitdelhi.ac.in</a>.
+              <p className="blue-300"> <a href="..\docs\PROGRAM_SCHEDULE_2.pdf" className="text-blue-500">Updated/ 2nd Draft of Tentative Programme Schedule</a> </p>
 
 
               </h1>
